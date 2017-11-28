d5c3704ca8df768d22bf9988ead6613d504182ea
Ngendo Muhayimana <ngendo.muhayimana@radio-canada.ca>
2017-11-27 20:13:26 -0500

    Use pdfkit to print timesheet

--- tSheet.py
+++ tSheet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import datetime
+import pdfkit
 from prettytable import PrettyTable
-from xhtml2pdf import pisa
 
 now        = datetime.date.today()
 lastMonth  = now.replace(month=now.month-1)
@@ -11,20 +11,12 @@ TO         = int(sys.argv[2])
 startDate  = lastMonth.replace(day=FROM)
 endDate    = lastMonth.replace(day=TO)
 
-print("Timesheet for %s -> %s"% (startDate, endDate))
-
-tContainer                  = PrettyTable()
-tContainer.border           = False
-tContainer.header           = False
-tColumn                     = [PrettyTable(),PrettyTable(),PrettyTable()]
-tContainer.add_row(tColumn)
-data                        = []
-
-#Add header for the day hours
-for i in range(0, len(tColumn) -1 ) :
-    tColumn[i].field_names = ["Day", "Hours"]
-
-tColumn[-1].field_names = ["Total Hours"]
+ioStreamTableContainer                  = PrettyTable()
+ioStreamTableContainer.border           = False
+ioStreamTableContainer.header           = False
+ioStreamTableColumn                     = [PrettyTable(),PrettyTable(),PrettyTable()]
+ioStreamTableContainer.add_row(ioStreamTableColumn)
+data                                    = []
 
 def isWeekend(date):
     return  date.weekday() >4 
@@ -34,38 +26,54 @@ for dayNum in range(FROM, TO+1):
     hours = 0.0 if isWeekend(day) else 7.5 
     data.append((dayNum,hours)) 
 
-for (dayNum, hours) in data:
-    tColumn[1 if dayNum > 15 else 0 ].add_row([dayNum, hours])
+sumHours = sum(x[1] for x in data) 
 
-tColumn[2].add_row([sum(row[1] for row in data)])
+def ioStreamPrint(data) :
+    #Add header for the day hours
+    for i in range(0, len(ioStreamTableColumn) -1 ) :
+        ioStreamTableColumn[i].field_names = ["Day", "Hours"]
+    
+    ioStreamTableColumn[-1].field_names = ["Total Hours"]
+    
+    for (dayNum, hours) in data:
+        ioStreamTableColumn[1 if dayNum > 15 else 0 ].add_row([dayNum, hours])
+    
+    ioStreamTableColumn[2].add_row([sumHours])
 
-print(tContainer)
-
-print("Consultant Signature:___________________\n")
-print("Client Signature:___________________\n")
+    print(ioStreamTableContainer)
 
 
-html = tContainer.get_html_string()
-# Define your data
-sourceHtml = "<html><body><p>Timesheet to PDF</p>"+ html +"</body></html>"
-outputFilename = "ttest.pdf"
+def htmlRow(dh) :
+    return "<td>%d</td><td>%.1f</td>" % (dh[0], dh[1])
 
-# Utility function
-def convertHtmlToPdf(sourceHtml, outputFilename):
-    # open output file for writing (truncated binary)
-    resultFile = open(outputFilename, "w+b")
+def htmlFromTotal(total) :
+    return ("<table border=1>" 
+                + "<th>Total Hours</th>"
+                + "<tr><td> %s </td></tr></table>" % total )
 
-    # convert HTML to PDF
-    pisaStatus = pisa.CreatePDF(
-            sourceHtml,                # the HTML to convert
-            dest=resultFile)           # file handle to recieve result
+def htmlFromData(data) :
+    day      = [htmlRow(x) for x in data]
+    headers  = ["Day","Hours"]
+    return ("<table border=1>" 
+                + "<th>Day</th><th>Hours</th>"
+                + "<tr>"
+                + "</tr><tr>".join(day) 
+                + "</tr></table>")
 
-    # close output file
-    resultFile.close()                 # close output file
+print("Timesheet for %s -> %s"% (startDate, endDate))
+ioStreamPrint(data)
 
-    # return True on success and False on errors
-    return pisaStatus.err
+#print("Consultant Signature:___________________\n")
+#print("Client Signature:___________________\n")
+col    = [
+        htmlFromData(data[:15]),
+        htmlFromData(data[15:]),
+        htmlFromTotal(sumHours)
+        ]
 
-# Main program
-convertHtmlToPdf(sourceHtml, outputFilename)
+resultHtml =  ("<table><tr><td>" 
+        + "</td><td>".join(col) 
+        + "</td></tr></table>")
+
+pdfkit.from_string( resultHtml , 'out.pdf')
 
