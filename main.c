1bcc7cd4e5200b9d3435d1f08eb58342248a1af2
Ngendo Muhayimana <ngendo.muhayimana@radio-canada.ca>
2017-11-29 20:08:30 -0500

    Created a make file

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "last_day.h"
 #include <stdio.h>
 #include <stdlib.h>     /* atoi */
 #include <time.h>
@@ -16,9 +17,7 @@ typedef struct {
     int total;
 } Table;
 
-
 void printValue(Table table) {
-    char *header[2] = {"Day", "Hours"};
     // print headers;
     int i;
     for(i = 0; i <table.total; i++ ) { 
@@ -82,47 +81,12 @@ Day_hours* day_hours(int from, int to, int month, int year) {
     for (i = 0 ; i < total ; i++) {
         int day = i + from;
         float hours = is_weekend(day, month, year) ? 7.5 :  0.0;
-        Day_hours dh  =  { day = day, .hours = hours } ;
+        Day_hours dh  =  { .day = day, .hours = hours } ;
         d_hours[i] =  dh ;
     }
 
     return d_hours;
 }
-
-
-int LastDay (int iMonth, int iYear)
-{
-    struct tm when;
-    time_t lastday;
-
-    // Set up current month
-    when.tm_hour = 0;
-    when.tm_min = 0;
-    when.tm_sec = 0;
-    when.tm_mday = 1;
-
-    // Next month 0=Jan
-    if (iMonth == 12)
-    {
-        when.tm_mon = 0;
-        when.tm_year = iYear - 1900 + 1;
-    }
-    else
-    {
-        when.tm_mon = iMonth;
-        when.tm_year = iYear - 1900;
-    }
-    // Get the first day of the next month
-    lastday = mktime (&when);
-
-    // Subtract 1 day
-    lastday -= 86400;
-
-    // Convert back to date and time
-    when = *localtime (&lastday);
-
-    return when.tm_mday;
-} 
 
 
 int main(int argc, char *argv[]) {
@@ -141,7 +105,7 @@ int main(int argc, char *argv[]) {
                 FROM, 
                 current_year, 
                 tm.tm_mon, 
-                LastDay(tm.tm_mon, current_year)
+                last_day(tm.tm_mon, current_year)
                 );
 
     Day_hours *dh = day_hours(FROM, TO, tm.tm_mon, current_year);
