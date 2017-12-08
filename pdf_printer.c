6ef8d488ece91053b31217363c459ef1b907bf27
Ngendo Muhayimana <ngendo.muhayimana@radio-canada.ca>
2017-12-07 21:09:48 -0500

    - Separate print_text into a dedicated function

--- pdf_printer.c
+++ pdf_printer.c
@@ -9,20 +9,22 @@
 jmp_buf env;
 
 void error_handler (HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data) {
-        printf ("ERROR: error_no=%04X, detail_no=%d\n",
-                      (unsigned int) error_no, (int) detail_no);
-            longjmp (env, 1); /* invoke longjmp() on error */
+    printf ("ERROR: error_no=%04X, detail_no=%d\n",
+            (unsigned int) error_no, (int) detail_no);
+    longjmp (env, 1); /* invoke longjmp() on error */
 }
 
 const int LINE_SPACE = 12;
 
 typedef struct frm frm;
+
 struct frm {
     int x;
     int y;    
 };
 
 typedef struct range range;
+
 struct range {
     int from;
     int to;
@@ -40,69 +42,18 @@ void print_frame(HPDF_Page page,  int x, int y, int height) {
     HPDF_Page_Stroke (page);
 }
 
-void print_value(HPDF_Page page, 
-        HPDF_Font font, 
-        Table table, 
-        range from_to,  
-        int x, int y) {
+void print_text(HPDF_Page page, HPDF_Font font, HPDF_Font font_bold) {
 
     struct nlist* result = read_rc_config();
 
-    int num_elem = from_to.to - from_to.from + 1 ;
+    HPDF_Page_BeginText(page);
 
-    print_frame(page, 
-            x,
-            400 - num_elem * LINE_SPACE,
-            (num_elem * LINE_SPACE) + 20
-            );
-
-    print_frame(page, 
-            x,
-            400 - num_elem * LINE_SPACE,
-            num_elem * LINE_SPACE
-            );
-
-    int top = 390; 
-
-    for(int i=0; i<2; i++ ) {
-        print_frame(page, 
-                300,
-                400 - 2 * LINE_SPACE,
-                (2 + i) * LINE_SPACE
-                );
-    }
-
-    /* print the title of the page (with positioning center). */
-    HPDF_Page_SetFontAndSize (page, font, 10);
-    HPDF_Page_BeginText (page);
-    char day[3]; 
-    char hours[7];
-
-    for(int i = from_to.from; i < from_to.to; i++ ) { 
-        sprintf(day, "%2d", table.day_hours[i].day);
-        sprintf(hours, "%3.1f", table.day_hours[i].hours);
-        int t = top - (i - from_to.from) * LINE_SPACE ;
-        HPDF_Page_TextOut (page, x + 10, t, day);
-        HPDF_Page_TextOut (page, x + 40, t, hours);
-    }
-
-    float sum_h = 0.0;
-    for( int i = 0; i < table.total; i++) 
-        sum_h += table.day_hours[i].hours;
-    
-    sprintf(hours, "%5.1f", sum_h );
-    HPDF_Page_TextOut (page, 320, 380, hours);
-    HPDF_Page_TextOut (page, 105, 405, "Date  Hours");
-    HPDF_Page_TextOut (page, 205, 405, "Date  Hours");
-    HPDF_Page_TextOut (page, 330, 405, "Total");
-
+    HPDF_Page_SetFontAndSize (page, font_bold, 11);
+    int cursor_height = HPDF_Page_GetHeight(page) - 30 ;
+    HPDF_Page_TextOut(page, 40, cursor_height  , "TimeSheet") ;
     
     HPDF_Page_SetFontAndSize (page, font, 7);
     HPDF_Page_TextOut(page, 40, 80,lookup("note")->defn);
-
-    HPDF_Page_SetFontAndSize (page, font, 11);
-    int cursor_height = HPDF_Page_GetHeight(page) - 30 ;
-    HPDF_Page_TextOut(page, 40, cursor_height  , "TimeSheet") ;
 
     HPDF_Page_SetFontAndSize (page, font, 10);
     cursor_height -= 20;
@@ -140,14 +91,70 @@ void print_value(HPDF_Page page,
     HPDF_Page_TextOut(page, 420, 130, "Client Signature:") ;
     HPDF_Page_TextOut(page, 500, 130, "____________________________") ;
 
-    HPDF_Page_EndText (page);
+    HPDF_Page_EndText(page);
+}
+
+void print_value(HPDF_Page page, 
+        HPDF_Font font, 
+        Table table, 
+        range from_to,  
+        int x, int y) {
+
+    int num_elem = from_to.to - from_to.from + 1 ;
+
+    print_frame(page, 
+            x,
+            400 - num_elem * LINE_SPACE,
+            (num_elem * LINE_SPACE) + 20
+            );
+
+    print_frame(page, 
+            x,
+            400 - num_elem * LINE_SPACE,
+            num_elem * LINE_SPACE
+            );
+
+    int top = 390; 
+
+    for(int i=0; i<2; i++ ) {
+        print_frame(page, 
+                300,
+                400 - 2 * LINE_SPACE,
+                (2 + i) * LINE_SPACE
+                );
+    }
+
+    HPDF_Page_SetFontAndSize(page, font, 10);
+    HPDF_Page_BeginText(page);
+
+    char day[3]; 
+    char hours[7];
+
+    for(int i = from_to.from; i < from_to.to; i++ ) { 
+        sprintf(day, "%2d", table.day_hours[i].day);
+        sprintf(hours, "%3.1f", table.day_hours[i].hours);
+        int t = top - (i - from_to.from) * LINE_SPACE ;
+        HPDF_Page_TextOut (page, x + 10, t, day);
+        HPDF_Page_TextOut (page, x + 40, t, hours);
+    }
+
+    float sum_h = 0.0;
+    for( int i = 0; i < table.total; i++) 
+        sum_h += table.day_hours[i].hours;
+    
+    sprintf(hours, "%5.1f", sum_h );
+    HPDF_Page_TextOut(page, 320, 380, hours);
+    HPDF_Page_TextOut(page, 105, 405, "Date  Hours");
+    HPDF_Page_TextOut(page, 205, 405, "Date  Hours");
+    HPDF_Page_TextOut(page, 330, 405, "Total");
+
+    HPDF_Page_EndText(page);
 }
 
 int pdf_printer(Table table) {
 
     HPDF_Doc pdf;
-    HPDF_Font font;
-    float tw;
+    HPDF_Font font, font_bold;
 
     pdf = HPDF_New (error_handler, NULL);
     if (!pdf) {
@@ -163,19 +170,20 @@ int pdf_printer(Table table) {
     /* set compression mode */
     HPDF_SetCompressionMode (pdf, HPDF_COMP_ALL);
 
-    /* create default-font */
-    font = HPDF_GetFont (pdf, "Helvetica", NULL);
-
     HPDF_Page page;
-
     page = HPDF_AddPage (pdf);
     HPDF_Page_SetSize (page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_LANDSCAPE);
+
+    font_bold = HPDF_GetFont(pdf, "Helvetica-Bold", NULL);
+    font = HPDF_GetFont(pdf, "Helvetica", NULL);
 
     range range[2] = { { .from = 0, .to = 15 },
                        { .from = 15, .to = table.total }
                          };
-    for(int i=0; i < 2; i++ ) {
 
+    print_text(page, font, font_bold);
+
+    for(int i=0; i < 2; i++ ) {
         print_value(page, 
                 font,
                 table,
